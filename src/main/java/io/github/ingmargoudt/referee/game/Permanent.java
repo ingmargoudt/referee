e7cd7a1a706a59a9d1626cf49595f6470cf28880
igoudt <igoudt@bol.com>
2021-05-30 14:20:07 +0200

    a permanent can tap

--- src/main/java/io/github/ingmargoudt/referee/game/Permanent.java
+++ src/main/java/io/github/ingmargoudt/referee/game/Permanent.java
@@ -3,9 +3,18 @@ package io.github.ingmargoudt.referee.game;
 public class Permanent extends MagicObject{
 
     Card card;
+    boolean isTapped;
 
     public Permanent(Card card){
         super();
         this.card = card;
+    }
+
+    public void tap(){
+        isTapped = true;
+    }
+
+    public void untap(){
+        isTapped = false;
     }
 }
