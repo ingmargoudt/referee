1e584803f8690f3e2fe7c0d539891cb8fcbb1fe4
igoudt <igoudt@bol.com>
2021-06-06 22:54:08 +0200

    110.5 , 110.5b, 110.5d

--- src/main/java/io/github/ingmargoudt/referee/game/Permanent.java
+++ src/main/java/io/github/ingmargoudt/referee/game/Permanent.java
@@ -5,7 +5,17 @@ import java.util.UUID;
 public class Permanent {
 
     Card card;
+
+    /*
+    110.5. A permanent’s status is its physical state. There are four status categories, each of which has two
+possible values: tapped/untapped, flipped/unflipped, face up/face down, and phased in/phased out.
+Each permanent always has one of these values for each of these categories.
+     */
     boolean isTapped;
+    boolean isFlipped;
+    boolean isFacedown;
+    boolean isPhasedOut;
+
 
     /*
     110.2. A permanent’s owner is the same as the owner of the card that represents it (unless it’s a token;
@@ -26,6 +36,30 @@ the battlefield. Every permanent has a controller.
 
     public void untap(){
         isTapped = false;
+    }
+
+    public void flip(){
+        isFlipped = true;
+    }
+
+    public void unflip(){
+        isFlipped = false;
+    }
+
+    public void faseUp(){
+        isFacedown = false;
+    }
+
+    public void faseDown(){
+        isFacedown = true;
+    }
+
+    public void phaseIn(){
+        isPhasedOut = false;
+    }
+
+    public void phaseOut(){
+        isPhasedOut = true;
     }
 
 
