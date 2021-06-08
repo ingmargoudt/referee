3c01f323062acbe066acc90522da3a4669c182d6
igoudt <igoudt@bol.com>
2021-06-08 22:41:24 +0200

    enable unit test by maven

--- src/main/java/io/github/ingmargoudt/referee/cards/g/GrizzlyBears.java
+++ src/main/java/io/github/ingmargoudt/referee/cards/g/GrizzlyBears.java
@@ -3,8 +3,6 @@ package io.github.ingmargoudt.referee.cards.g;
 import io.github.ingmargoudt.referee.game.Card;
 import io.github.ingmargoudt.referee.game.CardType;
 
-import java.util.UUID;
-
 public class GrizzlyBears extends Card {
     public GrizzlyBears() {
         super("Grizzly Bears");
