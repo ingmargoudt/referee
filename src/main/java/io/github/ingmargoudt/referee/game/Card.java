e09d78c57e0984d216dd686157791d6f0da49117
igoudt <igoudt@bol.com>
2021-10-04 19:22:35 +0200

    remove copy constructor for Card

--- src/main/java/io/github/ingmargoudt/referee/game/Card.java
+++ src/main/java/io/github/ingmargoudt/referee/game/Card.java
@@ -9,8 +9,7 @@ import java.util.UUID;
 
 @Getter
 public class Card extends MagicObject implements Targetable{
-
-    @Setter
+    
     UUID owner;
 
     public Card(String name){
@@ -25,22 +24,6 @@ public class Card extends MagicObject implements Targetable{
 
     public boolean isPermanent() {
         return getCardtypes().isPermanent();
-    }
-
-    public Card copy(){
-        return new Card(this);
-    }
-
-    private Card(Card card){
-        super(card.getName());
-        this.setController(card.getController());
-        this.owner = card.getOwner();
-        this.setPower(card.getPower());
-        this.setToughness(card.getToughness());
-        this.getAbilities().addAll(card.getAbilities());
-        this.getCardtypes().addAll(card.getCardtypes());
-        this.getSubTypes().addAll(card.getSubTypes());
-        this.getSpellEffects().addAll(card.getSpellEffects());
     }
 
     public boolean canBePlayed(Game game){
