3fbf4ba724c862d273a0c9eaace271de35025ee4
igoudt <igoudt@bol.com>
2021-12-29 14:46:46 +0100

    Implement Devastate

---
+++ src/main/java/io/github/ingmargoudt/referee/cards/d/Devastate.java
@@ -0,0 +1,51 @@
+package io.github.ingmargoudt.referee.cards.d;
+
+import io.github.ingmargoudt.referee.game.CardType;
+import io.github.ingmargoudt.referee.game.Game;
+import io.github.ingmargoudt.referee.game.SubType;
+import io.github.ingmargoudt.referee.game.effects.OneShotEffect;
+import io.github.ingmargoudt.referee.game.effects.TargetEffect;
+import io.github.ingmargoudt.referee.game.objects.Card;
+import io.github.ingmargoudt.referee.game.objects.MagicObject;
+import io.github.ingmargoudt.referee.game.objects.Permanent;
+import io.github.ingmargoudt.referee.game.targets.Filter;
+import io.github.ingmargoudt.referee.game.targets.TargetPermanent;
+
+import java.util.stream.Collectors;
+
+import static io.github.ingmargoudt.referee.game.targets.OrPredicate.or;
+
+public class Devastate extends Card {
+    public Devastate() {
+        super("Devastate");
+        cardtypes.add(CardType.SORCERY);
+        getSpellEffects().addEffect(new DevastateEffect());
+    }
+}
+
+class DevastateEffect extends OneShotEffect implements TargetEffect {
+
+    public DevastateEffect() {
+        targets.add(new TargetPermanent(Filter.by(CardType.LAND)));
+    }
+
+    @Override
+    public void apply(MagicObject object, Game game) {
+        game.getPlayer(object.getController()).ifPresent(controllerOfSpell -> {
+            targets.forEach(target -> {
+                target.resolve(game).ifPresent(targetable -> {
+                    if (targetable instanceof Permanent) {
+                        ((Permanent) targetable).destroy(game);
+                        game.getBattlefield().getAll().stream().filter(MagicObject::isCreature).collect(Collectors.toList()).forEach(creature -> creature.damage(controllerOfSpell, object, 1));
+                        game.getPlayers().forEach(player -> player.damage(controllerOfSpell, object, 1));
+                    }
+                });
+            });
+        });
+    }
+
+    @Override
+    public String getRule() {
+        return "Destroy " + targets.get(0).getRule() + ". {this} deals 1 damage to each creature and each player";
+    }
+}
---
+++ src/test/java/io/github/ingmargoudt/referee/cards/d/Test_Devastate.java
@@ -0,0 +1,34 @@
+package io.github.ingmargoudt.referee.cards.d;
+
+import io.github.ingmargoudt.referee.base.BaseGame;
+import io.github.ingmargoudt.referee.cards.c.Cryoclasm;
+import io.github.ingmargoudt.referee.cards.g.GrizzlyBears;
+import io.github.ingmargoudt.referee.cards.o.AlphaMyr;
+import io.github.ingmargoudt.referee.cards.p.Plains;
+import io.github.ingmargoudt.referee.game.Phase;
+import io.github.ingmargoudt.referee.game.objects.Card;
+import io.github.ingmargoudt.referee.game.zones.Zone;
+import org.junit.jupiter.api.Test;
+
+public class Test_Devastate extends BaseGame {
+
+    @Test
+    void destroyPlains() {
+        Card plains = new Plains();
+        Card bears = new GrizzlyBears();
+        Card devastate = new Devastate();
+        Card alphaMyr = new AlphaMyr();
+        addCard(Zone.BATTLEFIELD, player2, plains);
+        addCard(Zone.HAND, player1, devastate);
+        addCard(Zone.BATTLEFIELD,player1, bears);
+        addCard(Zone.BATTLEFIELD,player2, alphaMyr);
+        castSpell(1, Phase.PRECOMBAT_MAINPHASE, player1, devastate, plains);
+        stopAt(1, Phase.PRECOMBAT_MAINPHASE);
+        start();
+        assertGraveyard(player2, plains);
+        assertGraveyard(player2, alphaMyr);
+        assertPermanent(Zone.BATTLEFIELD, player1, bears, 1);
+        assertLife(player2, 19);
+        assertLife(player1, 19);
+    }
+}
